1fa0aacf5eff7cf93a1de65244c546a49532b817
MD-494 <mansidangat2004@gmail.com>
2025-09-21 20:56:22 +0530

    For loop and Nested for loop

--- ForLoopAndNestedLoop/src/forloop/PrintNumber.java
+++ ForLoopAndNestedLoop/src/forloop/PrintNumber.java
@@ -1,10 +1,18 @@
 package forloop;
 
+import java.util.Scanner;
+
 public class PrintNumber {
     static void main(String[] args) {
 
-        // 'for' loop to print numbers from 1 to 10
-        for(int i = 1; i <= 10; i++){
+        // Accept input from user
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter Any Number: ");
+        int n = sc.nextInt();
+
+        // 'for' loop to print numbers from 1 to n
+        for(int i = 1; i <= n; i++){
             System.out.println(i);
         }
     }
--- ForLoopAndNestedLoop/src/nestedforloop/MultiplicationTable.java
+++ ForLoopAndNestedLoop/src/nestedforloop/MultiplicationTable.java
@@ -4,7 +4,7 @@ public class MultiplicationTable {
     static void main(String[] args) {
 
         // Print the tables from 1 to 10
-        for(int i = 2; i <= 5; i++) {
+        for(int i = 1; i <= 10; i++) {
             for (int j = 1; j <= 10; j++) {
                 System.out.print(i * j + " ");
             }
